c7a818a660691e3ae98bccffb66b4771999c0899
FuckBug <48942627+FuckBug@users.noreply.github.com>
2020-04-10 02:02:09 +0800

    Fix some bugs

--- Jandan/Jandan.py
+++ Jandan/Jandan.py
@@ -99,6 +99,8 @@ def pic_link():
 '下载图片'
 def pic_down():
     down_link = pic_link()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
 
     # 检测是否指定下载目录,如果没有则默认在该文件目录下创建JandanPic文件夹
     if not os.path.exists('./JandanPic'):
@@ -107,10 +109,14 @@ def pic_down():
 
     pic_sum = len(down_link)
     print('本页共抓取到{}张图片链接\n'.format(pic_sum))
-
+    # a = 本页下载数, n = 总下载数
     a = 0
+    n = 0
     for link in down_link:
-        link = 'http://' + link
+        # 2020.4更新(图片链接域名ws1/ws2.sinaimg.cn出现无法访问的情况;更换二级域名的前缀即可;
+        link = link.split('.',1)
+        link = 'https://wx1.' + link[1]
+
         print(link)
         a += 1
 
@@ -121,7 +127,7 @@ def pic_down():
         pic_name = str(pic_name[0])
         print("图片名称:{}".format(pic_name))
 
-
+        a = a + 1  # 计数
         # 判断图片是否已经下载过
         # 先创建一个空文本,以防万一
         with open('./Downloads.txt', 'a') as f:
@@ -131,7 +137,7 @@ def pic_down():
 
             download_path = './JandanPic'
             if data == []:
-                print('OK,Downloading')
+                print('OK,Downloading... 正在下载第{}页第{}张图片 '.format(page_num,a))
 
 
                 # 判断图片是jpg还是gif格式,并进行下载
@@ -139,16 +145,18 @@ def pic_down():
                 if (pic_type):
                     # urllib.request.urlretrieve(link, download_path + "/{}.jpg".format(pic_name))
 
-                    down = requests.get(link)
+                    down = requests.get(link,headers=headers,timeout = 15)
                     with open(download_path + "/" + pic_name + ".jpg", 'wb')as f:
                         f.write(down.content)
-                    print("OK,已下载本页第{}张图片,本页还剩{}张待下载\n".format(a, (pic_sum - a)))
+                    n = n + 1   #计数
+                    print("OK,目前共下载{}张,已下载本页第{}张图片,本页还剩{}张待下载\n".format(n, a, (pic_sum - a)))
                 else:
                     # urllib.request.urlretrieve(link, download_path + "/{}.gif".format(pic_name))
-                    down = requests.get(link)
+                    down = requests.get(link,headers=headers,timeout = 15)
                     with open(download_path + "/" + pic_name + ".gif" , 'wb')as f:
                         f.write(down.content)
-                    print("OK,已下载本页第{}张图片,本页还剩{}张待下载\n".format(a, (pic_sum - a)))
+                    n = n + 1   #计数
+                    print("OK,目前共下载{}张,已下载本页第{}张图片,本页还剩{}张待下载\n".format(n,a, (pic_sum - a)))
 
                 # 将链接写入到文本中
                 with open('./Downloads.txt', 'a') as f:
@@ -156,21 +164,22 @@ def pic_down():
 
             else:
                 print('已跳过,因为该图片已经下载过\n')
-    print("下载完成!!!本页共下载{}张图片\n\n".format((a)))
+    print("本页任务完成!!!本页共下载或已下载过{}张图片\n\n".format((a)))
 
 
 '循环下载图片'
 def main(num):
     print("开始运行!")
     url = "http://jandan.net/pic"
+
     a = 1
     while a <= num:
-        time.sleep(0.5)
+        time.sleep(1)
         a += 1
         web_pages(url)
 
         pic_down()
-        time.sleep(0.5)
+        time.sleep(1)
         url = page_link
     print("任务完成!")
 
@@ -180,7 +189,7 @@ if __name__ == '__main__':
 ####################################################################################
                                                                                    #
                                                                                    #
-        '请在此输入想要爬取的页面个数,并确保您输入的是大于0的整数'                          #
+        '请在此输入想要爬取的页面个数,并确保您输入的是大于0的整数'                         #
         num = 100                                                                  #
                                                                                    #
                                                                                    #
@@ -188,3 +197,5 @@ if __name__ == '__main__':
         main(num)
     except KeyboardInterrupt:
         print("\n\n\n已手动停止运行!!!!!!")
+    except TimeoutError:
+        print("\n\n\n网络访问超时,可能此次的下载量过多!!!!!!")
